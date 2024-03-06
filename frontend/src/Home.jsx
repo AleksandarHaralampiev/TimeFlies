75f46556c4ce61e6425a62bcd57633887e19007e
MrTiNnN <martin.v.velchev@gmail.com>
2024-03-07 00:59:37 +0200

    Homee

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,8 +1,20 @@
+import compass from './img/compass.png'
+
 const Home = () => {
     return (
-        <main className="home-main">
-            <h1 className="heading-primary">Write your own history</h1>
-            <p className="home-text">Collect and share your stories with people all over the world. Create your own exciting timeline and collaborate with your friends and family.</p>
+        <main className="section-main">
+            <div className="container main-container">
+                <div className="main-textbox">
+                    <h1 className="heading-primary">Write your own history</h1>
+                    <p className="main-text">Collect and share your stories with people all over the world. Create your own exciting timeline and collaborate with your friends and family.</p>
+
+                    <button className="btn">Start now</button>
+                </div>
+
+                <div className="main-imgbox">
+                    <img src={compass} className='main-img'/>
+                </div>
+            </div>
         </main>
     )
 }
