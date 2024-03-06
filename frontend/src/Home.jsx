fc7b3c6de5d7dc8191a474f7a12d9d5601ac34ba
MrTiNnN <martin.v.velchev@gmail.com>
2024-03-06 23:57:36 +0200

    Home

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,6 +1,9 @@
 const Home = () => {
     return (
-        <main>Home</main>
+        <main className="home-main">
+            <h1 className="heading-primary">Write your own history</h1>
+            <p className="home-text">Collect and share your stories with people all over the world. Create your own exciting timeline and collaborate with your friends and family.</p>
+        </main>
     )
 }
 
---
+++ frontend/src/content.md
@@ -0,0 +1,3 @@
+## HOME PAGE
+# Write your own history
+Collect and share your stories with people all over the world. Create your own exciting timeline and collaborate with your friends and family.
