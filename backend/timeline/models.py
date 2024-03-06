6af139cd43a223944eb78219bb46442cecce3111
Aleksandar Haralampiev <alek.h.haralampiev@gmail.com>
2024-03-06 10:50:01 +0200

    cm

--- backend/timeline/models.py
+++ backend/timeline/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from ..server.models import Server
 
-# Create your models here.
+class Timeline(models.Model):
+    server = models.ForeignKey(Server, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date_modifired = models.DateTimeField(auto_now=True)
+    points = models.ManyToManyField('Point', related_name='timelines')
+    
+    class Meta:
+        db_table = "timelines"
+        ordering = ["-date"]
+    def __str__(self):
+        return self.title
+    
