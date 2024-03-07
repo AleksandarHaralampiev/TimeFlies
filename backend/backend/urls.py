0c73cfe849773b6d750b7a6ce588f6194ea77802
Vasil Vasilev <89039078+SookX@users.noreply.github.com>
2024-03-07 21:46:58 +0200

    Login/Register endpoints

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('authenticate/', include('authenticate.urls'))
 ]
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name = 'index.html'))]
