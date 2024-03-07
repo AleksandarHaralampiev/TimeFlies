99098db801c53cb82656ed11961dafd91646939b
Aleksandar Haralampiev <alek.h.haralampiev@gmail.com>
2024-03-07 20:48:11 +0200

    fix models woohooo

--- backend/timeline/models.py
+++ backend/timeline/models.py
@@ -1,16 +1,17 @@
 from django.db import models
-from ..server.models import Server
+import sys
+
+from server.models import Server
 
 class Timeline(models.Model):
     server = models.ForeignKey(Server, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField()
     date_modifired = models.DateTimeField(auto_now=True)
-    points = models.ManyToManyField('Point', related_name='timelines')
+
     
     class Meta:
         db_table = "timelines"
-        ordering = ["-date"]
     def __str__(self):
         return self.title
     
