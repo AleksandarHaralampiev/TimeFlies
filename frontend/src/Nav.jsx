e1e9f221a3c582a9798881c3a89b9d426aa4336e
MrTiNnN <martin.v.velchev@gmail.com>
2024-03-07 21:32:42 +0200

    Dropdown

--- frontend/src/Nav.jsx
+++ frontend/src/Nav.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { DataContext } from './context/DataContext'
 
@@ -6,6 +6,8 @@ import pfp from './img/pfp.jpg'
 
 const Nav = () => {
     const { loggedIn } = useContext(DataContext)
+
+    const [dropdown, setDropdown] = useState(false)
     
     return (
         <nav className="navbar">
@@ -21,24 +23,32 @@ const Nav = () => {
                 </li>
             </ul>
 
-            <ul className="nav-links">
-                {
-                    loggedIn ?
-                    <li>
-                        <img src={pfp} className="pfp" />
-                    </li>
-                    :
-                    <>
-                        <li>
-                            <Link to='/login' className='nav-link'>Log In</Link>
-                        </li>
-                        <li>
-                            <Link to='/register' className='nav-link'>Sign Up</Link>
-                        </li>
-                    </>
-                }
-            </ul>
+            {
+                loggedIn ?
+                <div className='pfp-container'>
+                    <img src={pfp} className="pfp" onClick={() => setDropdown(!dropdown)}/>
 
+                    {
+                        dropdown ?
+                        <ul className="dropdown">
+                            <li>My Profile</li>
+                            <li>My Timelines</li>
+                            <li>Sign Out</li>
+                        </ul>
+                        :
+                        null
+                    }
+                </div>
+                :
+                <ul className='nav-links'>
+                    <li>
+                        <Link to='/login' className='nav-link'>Log In</Link>
+                    </li>
+                    <li>
+                        <Link to='/register' className='nav-link'>Sign Up</Link>
+                    </li>
+                </ul>
+            }
 
         </nav>
     )
