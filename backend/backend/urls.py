6759adef2e7331d24cc99589651611a428316d2a
Vasil Vasilev <89039078+SookX@users.noreply.github.com>
2024-03-08 22:09:41 +0200

    View timeline

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('server/', include('server.urls')),
     path('authenticate/', include('authenticate.urls'))
 ]
 
---
+++ backend/server/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+from . import views
+
+
+
+urlpatterns = [
+    path('create/', views.createTimeLine)
+]
+
