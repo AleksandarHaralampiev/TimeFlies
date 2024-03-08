30dcd2f738b7e9f5d1330af72b8b02d56bc6dfdc
MrTiNnN <martin.v.velchev@gmail.com>
2024-03-08 19:25:58 +0200

    Dashboard Finished

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { DataContext } from "./context/DataContext"
 import { Link } from "react-router-dom"
 
@@ -8,6 +8,8 @@ const Dashboard = () => {
     const { loggedIn, navigate } = useContext(DataContext)
 
     const profiles = Array.from({ length: 12 }, () => pfp)
+
+    const [search, setSearch] = useState('')
 
     const timelines = [
         {
@@ -32,7 +34,7 @@ const Dashboard = () => {
         },
         {
             name: "Timeline 1",
-            description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae cum facilis laudantium consectetur perspiciatis doloremque eveniet ab, quibusdam voluptate sapiente nemo neque, eius molestias commodi eligendi labore illo, error magni corporis. Praesentium deserunt asperiores dignissimos autem, a, et eveniet quod at perferendis, accusantium tenetur quia omnis minus consectetur ratione harum."
+            description: "Loorem ipsum dolor sit amet consectetur, adipisicing elit. Quae cum facilis laudantium consectetur perspiciatis doloremque eveniet ab, quibusdam voluptate sapiente nemo neque, eius molestias commodi eligendi labore illo, error magni corporis. Praesentium deserunt asperiores dignissimos autem, a, et eveniet quod at perferendis, accusantium tenetur quia omnis minus consectetur ratione harum."
         },
         {
             name: "Timeline 1",
@@ -44,40 +46,73 @@ const Dashboard = () => {
         }
     ]
 
+    const [shownTimelines, setShownTimelines] = useState([])
+
+    useEffect(() => {
+        setShownTimelines(timelines.filter(timeline => 
+            timeline.name.toLowerCase().includes(search.toLowerCase()) || timeline.description.toLowerCase().includes(search.toLowerCase())
+        ))
+    }, [search])
+
     useEffect(() => {
         if(!loggedIn) navigate('/login')
     }, [loggedIn])
     
     return (
         <section className="section-dashboard">
-            <div className="container timeline-grid">
-                {
-                    timelines.map(timeline => (
-                        <div className="timeline-container" onClick={() => navigate('/timeline')}>
-                            
-                            <h3 to='/timeline' className="timeline-name">{timeline.name}</h3>
+            <div className="container">
+                <div className="text-box">
+                    <h1 className="dashboard-heading">Dashboard</h1>
 
-                            <p className="timeline-description">
-                                {
-                                    timeline.description.length < 100 ?
-                                    timeline.description
-                                    :
-                                    `${timeline.description.slice(0, 100)}...`
-                                }
-                            </p>
+                    <p className="dashboard-text">All available timelines are displayed here. Choose one and start building upon it!</p>
+                </div>
 
-                            <div className="timeline-img-box">
-                                {
-                                    profiles.slice(0, 5).map(pic => (
-                                        <img src={pic} className="timeline-pfp"/>
-                                    ))
-                                }
-                            </div>
-
-                        </div>
-                    ))
-                }
+                <div className="search-box">
+                    <input type="text" placeholder="Search..." value={search} onChange={(e) => setSearch(e.target.value)}/>
+                </div>
             </div>
+
+                {
+                    timelines.length ?
+                    shownTimelines.length ?
+                        <div className="container timeline-grid">
+                            {
+                                shownTimelines.map(timeline => (
+                                    <div className="timeline-container" onClick={() => navigate('/timeline')}>
+                                        
+                                        <h3 to='/timeline' className="timeline-name">{timeline.name}</h3>
+
+                                        <p className="timeline-description">
+                                            {
+                                                timeline.description.length < 100 ?
+                                                timeline.description
+                                                :
+                                                `${timeline.description.slice(0, 100)}...`
+                                            }
+                                        </p>
+
+                                        <div className="timeline-img-box">
+                                            {
+                                                profiles.slice(0, 5).map(pic => (
+                                                    <img src={pic} className="timeline-pfp"/>
+                                                ))
+                                            }
+
+                                        </div>
+
+                                    </div>
+                                ))
+                            }
+                        </div>
+                        :
+                        <div className="container">
+                            <p className="dashboard-text">No result.</p>
+                        </div>
+                    :
+                    <div className="container">
+                        <p className="dashboard-empty">No timelines yet. Perhaps you can <Link to=''>create your own</Link></p>
+                    </div>
+                }
         </section>
     )
 }
