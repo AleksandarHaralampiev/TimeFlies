6ef4d93882ebe2d391d6f3b7337674177e2c4a03
anastassow <127148449+anastassow@users.noreply.github.com>
2024-03-10 01:15:57 +0200

    view for the profile image and url for react to sent data

--- backend/authenticate/urls.py
+++ backend/authenticate/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from . import views
+from .views import PostView
 
 
 
 urlpatterns = [
     path('valid/', views.isUser),
-    path('info/', views.getUserCredentials)
+    path('info/', views.getUserCredentials),
+    path('api/posts/', PostView.as_view(), name='posts'),
 ]
 
--- backend/authenticate/views.py
+++ backend/authenticate/views.py
@@ -6,6 +6,10 @@ from rest_framework.response import Response
 from .models import UserAccount
 from base64 import b64encode
 import mimetypes
+from rest_framework import status
+from rest_framework.views import APIView
+from .models import UserAccount
+from .serializers import UserCreateSerializer
 
 @api_view(['POST', 'GET'])
 def isUser(request, *args, **kwargs):
@@ -42,3 +46,56 @@ def getUserCredentials(request, *args, **kwargs):
             return Response(data = {"data": user_data}, status=200)
         except:
             return Response(data = {"Message": "There is no such account"}, status=404)
+
+@api_view(['POST', 'GET'])
+def isUser(request, *args, **kwargs):
+    body = request.body
+    data = json.loads(body)
+    email = data['email']
+    password = data['password']
+    try:
+        user = UserAccount.objects.get(email=email)
+    except UserAccount.DoesNotExist:
+        return Response(data={"message": "This account doesn't exist."}, status=404)
+    if check_password(password, user.password):
+        return Response(data = {"id": str(user.id)}, status=200)
+    else:
+        return Response(data = {"message": "Wrong password."}, status=404)
+    
+@api_view(['GET'])
+def getUserCredentials(request, *args, **kwargs):
+    if request.method == "GET":
+        id = int(request.GET.get('id'))
+        try:
+            user = UserAccount.objects.filter(id = id).first()
+            user_data = {
+                "email": user.email,
+                "username": user.username,
+                "profile_picture": None  
+            }
+            if user.profile_picture:
+                content_type, _ = mimetypes.guess_type(user.profile_picture.name)
+                with user.profile_picture.open('rb') as image_file:
+                    encoded_string = b64encode(image_file.read()).decode('utf-8')
+                    data_uri = f"data:{content_type};base64,{encoded_string}"
+                    user_data["profile_picture"] = data_uri
+            return Response(data = {"data": user_data}, status=200)
+        except:
+            return Response(data = {"Message": "There is no such account"}, status=404)
+
+class PostView(APIView):
+    def get(self, request, *args, **kwargs):
+        posts = UserAccount.objects.all()
+        serializer = UserCreateSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        posts_serializer = UserCreateSerializer(data=request.data)
+        if posts_serializer.is_valid():
+            posts_serializer.save()
+            return Response(posts_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            print('error', posts_serializer.errors)
+            return Response(posts_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
